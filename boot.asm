a93101a0ac827e35bb8e65381cdf09c757a22be6
Omar <omarbelghaouti@gmail.com>
2024-05-30 10:26:04 +0200

    switching to protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
 	jmp short start
@@ -8,57 +11,68 @@ _start:
 times 33 db 0
 
 start:
-	jmp 0x7c0:step2
+	jmp 0:step2
 
 
 step2:
 	cli ; clear interrupts
-	mov ax, 0x7c0
+	mov ax, 0x00
 	mov ds, ax
 	mov es, ax
-	mov ax, 0x00
 	mov ss, ax
 	mov sp, 0x7c00
 	sti ; enable interrupts
 
-	mov ah, 2 ; read sector command
-	mov al, 1 ; one sector to read
-	mov ch, 0 ; cylinder low 8 bits
-	mov cl, 2 ; read sector 2
-	mov dh, 0 ; head number
-	mov bx, buffer
-	int 0x13 ; invoke read command
-	jc error ; if carry flag is set, jump to error
-	
-	mov si, buffer
-	call print
+.load_protected:
+	cli
+	lgdt[gdt_descriptor]
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax
+	jmp CODE_SEG:load32
 
+; GDT
+gdt_start:
+gdt_null:
+	dd 0x0
+	dd 0x0
+
+; offset 0x8
+gdt_code:	  ; CS should point to this
+	dw 0xffff ; segment limit first 0-15 bits
+	dw 0	  ; base first 0-15 bits
+	db 0	  ; base 16-23 bits
+	db 0x9a	  ; access byte
+	db 11001111b ; high 4 bit flags and low 4 bit flags
+	db 0	  ; base 24-31 bits
+
+; offset 0x10
+gdt_data:	  ; DS, SS, ES, FS, GS
+	dw 0xffff ; segment limit first 0-15 bits
+	dw 0	  ; base first 0-15 bits
+	db 0	  ; base 16-23 bits
+	db 0x92	  ; access byte
+	db 11001111b ; high 4 bit flags and low 4 bit flags
+	db 0	  ; base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1
+	dd gdt_start
+
+[BITS 32]
+load32:
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	mov ebp, 0x00200000
+	mov esp, ebp
 	jmp $
-
-error:
-	mov si, error_message
-	call print
-	jmp $
-
-print:	
-	mov bx, 0
-.loop:
-	lodsb
-	cmp al, 0
-	je .done
-	call print_char
-	jmp .loop
-.done:
-	ret
-
-print_char:
-	mov ah, 0eh
-	int 0x10
-	ret
-
-error_message: db 'Failed to load sector', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
 
-buffer:
