cee52c233b612d275e837af87c94f1da316f3327
Omar <omarbelghaouti@gmail.com>
2024-07-12 22:23:19 +0200

    impl kernel panic

--- src/kernel.c
+++ src/kernel.c
@@ -67,6 +67,14 @@ void print(const char *str)
 
 static struct paging_4gb_chunk *kernel_chunk = 0;
 
+void panic(const char *msg)
+{
+	print(msg);
+	while (42)
+	{
+	}
+}
+
 void kernel_main()
 {
 	terminal_init();
