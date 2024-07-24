0f6ff32725bd25c21e672730eeb289c81dcdf736
Omar <omarbelghaouti@gmail.com>
2024-07-24 22:29:59 +0200

    finishing ps2 port keyboard driver

--- src/keyboard/ps2.c
+++ src/keyboard/ps2.c
@@ -3,8 +3,12 @@
 #include "io/io.h"
 #include <stdint.h>
 #include <stddef.h>
+#include "kernel.h"
+#include "idt/idt.h"
+#include "task/task.h"
 
 int ps2_keyboard_init();
+void ps2_keyboard_handle_interrupt();
 
 static uint8_t keyboard_scan_set_one[] = {
 	0x00, 0x1b, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
@@ -21,6 +25,7 @@ struct keyboard ps2_keyboard = {
 
 int ps2_keyboard_init()
 {
+	idt_register_interrupt_callback(ISR_KEYBOARD_INTERRUPT, ps2_keyboard_handle_interrupt);
 	outb(PS2_PORT, PS2_COMMAND_ENABLE_FIRST_PORT);
 	return 0;
 }
@@ -39,6 +44,22 @@ uint8_t ps2_keyboard_scancode_to_char(uint8_t scancode)
 
 void ps2_keyboard_handle_interrupt()
 {
+	kernel_page();
+	uint8_t scancode = insb(KEYBOARD_INPUT_PORT);
+	insb(KEYBOARD_INPUT_PORT);
+
+	if (scancode & PS2_KEYBOARD_KEY_RELEASED)
+	{
+		return;
+	}
+
+	uint8_t c = ps2_keyboard_scancode_to_char(scancode);
+	if (c != 0)
+	{
+		keyboard_push(c);
+	}
+
+	task_page();
 }
 
 struct keyboard *ps2_init()
