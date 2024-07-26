16f0c5e17e0264a2eac296476d448e654a2b93d2
Omar <omarbelghaouti@gmail.com>
2024-07-26 23:35:23 +0200

    fix stream reader

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,6 +1,7 @@
 #include "streamer.h"
 #include "memory/heap/kheap.h"
 #include "config.h"
+#include <stdbool.h>
 
 struct disk_stream *diskstreamer_new(int disk_id)
 {
@@ -26,7 +27,14 @@ int diskstreamer_read(struct disk_stream *stream, void *out, int total)
 {
 	int sector = stream->pos / MYOS_SECTOR_SIZE;
 	int offset = stream->pos % MYOS_SECTOR_SIZE;
+	int total_to_read = total;
+	bool overflow = (offset + total_to_read) >= MYOS_SECTOR_SIZE;
 	char buf[MYOS_SECTOR_SIZE];
+
+	if (overflow)
+	{
+		total_to_read -= (offset + total_to_read) - MYOS_SECTOR_SIZE;
+	}
 
 	int res = disk_read_block(stream->disk, sector, 1, buf);
 	if (res < 0)
@@ -34,7 +42,6 @@ int diskstreamer_read(struct disk_stream *stream, void *out, int total)
 		goto out;
 	}
 
-	int total_to_read = total > MYOS_SECTOR_SIZE ? MYOS_SECTOR_SIZE : total;
 	for (int i = 0; i < total_to_read; i++)
 	{
 		*(char *)out++ = buf[offset + i];
@@ -42,9 +49,9 @@ int diskstreamer_read(struct disk_stream *stream, void *out, int total)
 
 	// adjust stream
 	stream->pos += total_to_read;
-	if (total > MYOS_SECTOR_SIZE)
+	if (overflow)
 	{
-		res = diskstreamer_read(stream, out, total - MYOS_SECTOR_SIZE);
+		res = diskstreamer_read(stream, out, total - total_to_read);
 	}
 
 out:
