e7bbf8d2a5addaebdbcd0c04a6048dabafa5cd9b
Omar <omarbelghaouti@gmail.com>
2024-07-28 17:32:52 +0200

    add useful functions in stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,20 +1,17 @@
 #include "myos.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
 int main(int argc, char **argv)
 {
-	print("Hello from user program!\n");
-	print(itoa(2024));
-
-	void *ptr = malloc(512);
-	free(ptr);
-
-	printf("\n%i bytes was allocated for %s\n", 512, "ptr");
-
-	char buf[1024];
-	myos_terminal_readline(buf, sizeof(buf), true);
-	print(buf);
+	char words[] = "hello from program";
+	const char *token = strtok(words, " ");
+	while (token)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, " ");
+	}
 
 	while (42)
 	{
