55c11ce9e6283352e340220b477a90bafdfda802
Omar <omarbelghaouti@gmail.com>
2024-07-28 23:31:54 +0200

    fix memory leak in process_load_binary

--- src/task/process.c
+++ src/task/process.c
@@ -307,6 +307,7 @@ void process_free(struct process *process, void *ptr)
 static int process_load_binary(const char *filename, struct process *process)
 {
 	int res = 0;
+	void *program_data_ptr = 0;
 	int fd = fopen(filename, "r");
 	if (!fd)
 	{
@@ -321,7 +322,7 @@ static int process_load_binary(const char *filename, struct process *process)
 		goto out;
 	}
 
-	void *program_data_ptr = kzalloc(stat.filesize);
+	program_data_ptr = kzalloc(stat.filesize);
 	if (!program_data_ptr)
 	{
 		res = -ENOMEM;
@@ -339,6 +340,13 @@ static int process_load_binary(const char *filename, struct process *process)
 	process->size = stat.filesize;
 
 out:
+	if (res < 0)
+	{
+		if (program_data_ptr)
+		{
+			kfree(program_data_ptr);
+		}
+	}
 	fclose(fd);
 	return res;
 }
