07e977a2fc0104ebe085a3fd7b43cabc4288c2ad
Omar <omarbelghaouti@gmail.com>
2024-07-28 12:58:28 +0200

    create shell program

---
+++ programs/shell/src/shell.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "myos.h"
+
+int main(int argc, char **argv)
+{
+	print("MYOS v1.0.0\n");
+	while (42)
+	{
+		print("$ ");
+		char buf[1024];
+		myos_terminal_readline(buf, sizeof(buf), true);
+		print("\n");
+	}
+
+	return 0;
+}
