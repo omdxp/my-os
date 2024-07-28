569dc4e1323a8e886e6322bf36daec5795d6758e
Omar <omarbelghaouti@gmail.com>
2024-07-28 19:26:20 +0200

    start main with arguments

---
+++ programs/stdlib/src/start.c
@@ -0,0 +1,13 @@
+#include "myos.h"
+
+extern int main(int argc, char **argv);
+
+void c_start()
+{
+	struct process_arguments arguments;
+	myos_process_get_arguments(&arguments);
+	int res = main(arguments.argc, arguments.argv);
+	if (res == 0)
+	{
+	}
+}
