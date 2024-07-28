4ffdbe8cc5038052b2b50c6ab22bee45a23b055b
Omar <omarbelghaouti@gmail.com>
2024-07-28 09:51:24 +0200

    impl stdlib system getkey function

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,6 +5,10 @@ int main(int argc, char **argv)
 	print("Hello from user program!\n");
 	while (42)
 	{
+		if (getkey() != 0)
+		{
+			print("key pressed!\n");
+		}
 	}
 
 	return 0;
--- programs/stdlib/src/myos.h
+++ programs/stdlib/src/myos.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void print(const char *filename);
+int getkey();
