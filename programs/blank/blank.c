bb84918696a707d7a6988dc75f9bc75c06204664
Omar <omarbelghaouti@gmail.com>
2024-07-28 17:43:26 +0200

    memory mapping malloc in stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,13 +5,9 @@
 
 int main(int argc, char **argv)
 {
-	char words[] = "hello from program";
-	const char *token = strtok(words, " ");
-	while (token)
-	{
-		printf("%s\n", token);
-		token = strtok(NULL, " ");
-	}
+	char *ptr = malloc(20);
+	strcpy(ptr, "Hello, World!");
+	print(ptr);
 
 	while (42)
 	{
