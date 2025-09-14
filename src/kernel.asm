9113134c779ebcf635ecedd3fb7914eeda19256d
Omar <omarbelghaouti@gmail.com>
2025-09-14 21:45:21 +0200

    refactor build system to support x86_64 target and update kernel assembly for long mode

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,12 +1,11 @@
 [BITS 32]
 
 global _start
-global kernel_registers
 
-extern kernel_main
-
+; segment selectors
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
+LONG_MODE_CODE_SEG equ 0x18
 
 _start:
 	mov ax, DATA_SEG
@@ -15,34 +14,91 @@ _start:
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
+
+	; setup stack
 	mov ebp, 0x00200000
 	mov esp, ebp
 
-	; enable A20 line
-	in al, 0x92
-	or al, 2
-	out 0x92, al
+	; load GDT
+	lgdt [gdt_descriptor]
 
-	; remap master PIC
-	mov al, 00010001b
-	out 0x20, al ; tell master PIC
+	; enable PAE in CR4
+	mov eax, cr4
+	or eax, 1 << 5
+	mov cr4, eax
 
-	mov al, 0x20 ; interrupt 0x20 is where master ISR should start
-	out 0x21, al
+	; setup page tables
+	mov eax, PML4_table
+	mov cr3, eax
 
-	mov al, 00000001b
-	out 0x21, al
-	; end remap master PIC
+	; enable long mode in EFER MSR
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
 
-	call kernel_main
+	; enable paging in CR0
+	mov eax, cr0
+	or eax, 1 << 31 ; set PG bit
+	or eax, 1 << 0  ; set PE bit
+	mov cr0, eax
+
+	; jump to 64-bit code segment
+	jmp LONG_MODE_CODE_SEG:long_mode_entry
+
+[BITS 64]
+long_mode_entry:
 	jmp $
 
-kernel_registers:
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov gs, ax
-	mov fs, ax
-	ret
+; global descriptor table (GDT)
+gdt:
+	; null segment
+	dq 0x0000000000000000
 
-times 512-($ - $$) db 0
+	; 32-bit code segment
+	dw 0xFFFF           ; limit low
+	dw 0x0000           ; base low
+	db 0x00             ; base middle
+	db 10011010b        ; access
+	db 11001111b        ; granularity
+	db 0x00             ; base high
+
+	; 32-bit data segment
+	dw 0xFFFF           ; limit low
+	dw 0x0000           ; base low
+	db 0x00             ; base middle
+	db 10010010b        ; access
+	db 11001111b        ; granularity
+	db 0x00             ; base high
+
+	; 64-bit code segment
+	dw 0x0000           ; limit low
+	dw 0x0000           ; base low
+	db 0x00             ; base middle
+	db 10011010b        ; access
+	db 00100000b        ; granularity (L bit set)
+	db 0x00             ; base high
+
+gdt_end:
+
+gdt_descriptor:
+	dw gdt_end - gdt - 1 ; size of GDT
+	dd gdt               ; address of GDT
+
+; page table definitions
+align 4096
+PML4_table:
+	dq PDPT_table + 0x03 ; present, read/write
+	times 511 dq 0       ; zero rest of table
+
+align 4096
+PDPT_table:
+	dq PD_table + 0x03   ; present, read/write
+	times 511 dq 0       ; zero rest of table
+
+align 4096
+PD_table:
+	; map first 4 MB of memory using 2 MB pages
+	dq 0x0000000000000083 ; first 2 MB, present, read/write, 2 MB page
+	dq 0x0000000000200083 ; second 2 MB, present, read/write, 2 MB page
+	times 510 dq 0        ; zero rest of table
