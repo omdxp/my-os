203ab23540f66d71b8a50241a700705cf83c0a5f
omdxp <omar@omdxp.com>
2025-09-20 19:17:42 +0200

    enhance kheap management with paging alignment; add kpalloc and kpzalloc functions for multiheap allocation

--- src/memory/heap/kheap.c
+++ src/memory/heap/kheap.c
@@ -3,6 +3,7 @@
 #include "config.h"
 #include "kernel.h"
 #include "memory/memory.h"
+#include "memory/paging/paging.h"
 #include "multiheap.h"
 
 struct heap kernel_minimal_heap;
@@ -43,6 +44,16 @@ void kheap_init()
 
 	void *heap_address = heap_table_address + MYOS_MINIMAL_HEAP_TABLE_SIZE;
 	void *heap_end_address = heap_address + MYOS_HEAP_SIZE_BYTES;
+	if (!paging_is_aligned(heap_address))
+	{
+		heap_address = paging_align_address(heap_address);
+	}
+
+	if (!paging_is_aligned(heap_end_address))
+	{
+		heap_end_address = paging_align_to_lower_page(heap_end_address);
+	}
+
 	size_t size = heap_end_address - heap_address;
 	size_t total_table_entries = size / MYOS_HEAP_BLOCK_SIZE;
 	kernel_minimal_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY *)heap_table_address;
@@ -73,7 +84,19 @@ void kheap_init()
 		struct e820_entry *current = e820_entry(i);
 		if (current->type == 1 && current != used_entru)
 		{
-			multiheap_add(kernel_multiheap, (void *)current->base_addr, (void *)(current->base_addr + current->length), MULTIHEAP_HEAP_FLAG_DEFRAGMENT_WITH_PAGING);
+			void *base_addr = (void *)current->base_addr;
+			void *end_addr = (void *)(current->base_addr + current->length);
+			if (!paging_is_aligned(base_addr))
+			{
+				base_addr = paging_align_address(base_addr);
+			}
+
+			if (!paging_is_aligned(end_addr))
+			{
+				end_addr = paging_align_to_lower_page(end_addr);
+			}
+
+			multiheap_add(kernel_multiheap, (void *)base_addr, (void *)end_addr, MULTIHEAP_HEAP_FLAG_DEFRAGMENT_WITH_PAGING);
 		}
 	}
 }
@@ -90,14 +113,34 @@ void *kmalloc(size_t size)
 
 void *kzalloc(size_t size)
 {
-	return NULL;
-	// void *ptr = kmalloc(size);
-	// if (!ptr)
-	// {
-	// 	return 0;
-	// }
-	// memset(ptr, 0x00, size);
-	// return ptr;
+	void *ptr = kmalloc(size);
+	if (!ptr)
+	{
+		return 0;
+	}
+	memset(ptr, 0x00, size);
+	return ptr;
+}
+
+void *kpalloc(size_t size)
+{
+	void *ptr = multiheap_palloc(kernel_multiheap, size);
+	if (!ptr)
+	{
+		return 0;
+	}
+	return ptr;
+}
+
+void *kpzalloc(size_t size)
+{
+	void *ptr = kpalloc(size);
+	if (!ptr)
+	{
+		return 0;
+	}
+	memset(ptr, 0x00, size);
+	return ptr;
 }
 
 void kfree(void *ptr)
