3710ebe571633782e34cd8a9a69e0ab8c627a0a2
omdxp <omar@omdxp.com>
2026-01-21 21:34:20 +0100

    feat: add window management structures and event handling definitions

---
+++ kernel/src/graphics/window.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "graphics/terminal.h"
+#include "graphics/graphics.h"
+#include "config.h"
+#include <stddef.h>
+#include <stdint.h>
+
+struct window;
+
+enum
+{
+	WINDOW_EVENT_TYPE_NULL,			// no event
+	WINDOW_EVENT_TYPE_FOCUS,		// window focus event
+	WINDOW_EVENT_TYPE_LOST_FOCUS,	// window lost focus event
+	WINDOW_EVENT_TYPE_MOUSE_MOVE,	// mouse move event
+	WINDOW_EVENT_TYPE_MOUSE_CLICK,	// mouse click event
+	WINDOW_EVENT_TYPE_WINDOW_CLOSE, // window close event
+	WINDOW_EVENT_TYPE_KEYPRESS,		// keypress event
+
+};
+
+struct window_event
+{
+	int type;			   // type of the event
+	int win_id;			   // id of the window generating the event
+	struct window *window; // pointer to the window generating the event
+	union
+	{
+		struct
+		{
+			// no additional data for close event
+		} focus; // focus event data
+
+		struct
+		{
+			int x; // x position of the mouse click
+			int y; // y position of the mouse click
+		} move;	   // mouse move event data
+
+		struct
+		{
+			int x; // x position of the mouse click
+			int y; // y position of the mouse click
+		} click;   // mouse click event data
+
+		struct
+		{
+			char key; // key that was pressed
+		} keypress;	  // keypress event data
+	} data;			  // event-specific data
+};
+
+typedef int (*WINDOW_EVENT_HANDLER)(struct window *window, struct window_event *event);
+
+enum
+{
+	WINDOW_FLAG_BORDERLESS = 0x1,			  // Window has no border
+	WINDOW_FLAG_CLICK_THROUGH = 0x2,		  // Window ignores mouse clicks
+	WINDOW_FLAG_BACKGROUND_TRANSPARENT = 0x4, // Window background is transparent
+};
+
+struct window
+{
+	int id;								 // unique identifier for the window
+	struct terminal *title_bar_terminal; // title bar terminal
+	struct terminal *terminal;			 // body terminal
+
+	struct graphics_info *root_graphics;	  // pointer to the root graphics_info (contains sub-graphics for the window)
+	struct graphics_info *title_bar_graphics; // pointer to the title bar graphics_info
+	struct graphics_info *graphics;			  // pointer to the body graphics_info
+
+	struct
+	{
+		struct
+		{
+			size_t x;
+			size_t y;
+			size_t width;
+			size_t height;
+		} close_button; // close button dimensions and position
+
+	} title_bar_components; // components specific to the title bar
+
+	struct
+	{
+		struct vector *handlers; // vector of WINDOW_EVENT_HANDLER*
+	} event_handlers;			 // event handlers for the window
+
+	size_t width;  // width of the window
+	size_t height; // height of the window
+
+	size_t x; // x position of the window
+	size_t y; // y position of the window
+
+	size_t z_index; // z-index of the window
+
+	char title[WINDOW_MAX_TITLE_LENGTH]; // title of the window
+	int flags;							 // flags for window behavior
+};
