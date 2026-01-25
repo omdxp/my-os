596ab1590ca1d7b2c921ccc4e5598676770b72c1
omdxp <omar@omdxp.com>
2026-01-25 23:25:33 +0100

    feat: enhance graphics handling by adding userland graphics structure and pixel manipulation

--- kernel/programs/blank/blank.c
+++ kernel/programs/blank/blank.c
@@ -11,6 +11,25 @@ struct window
 	int height;
 };
 
+struct framebuffer_pixel
+{
+	uint8_t blue;
+	uint8_t green;
+	uint8_t red;
+	uint8_t reserved;
+};
+
+struct userland_graphics
+{
+	size_t x;
+	size_t y;
+	size_t width;
+	size_t height;
+
+	void *pixels;		// pixels array
+	void *userland_ptr; // pointer to graphics in userland process
+};
+
 int main(int argc, char **argv)
 {
 	struct window *win = myos_window_create("Blank Program", 800, 600, 0, 0);
@@ -25,6 +44,25 @@ int main(int argc, char **argv)
 
 	// divert stdout to the created window
 	myos_divert_stdout_to_window(win);
+
+	struct userland_graphics *graphics = myos_window_get_graphics(win);
+	if (!graphics)
+	{
+		printf("Failed to get graphics context for window\n");
+		return -1;
+	}
+
+	struct framebuffer_pixel *pixels = myos_graphics_get_pixels(graphics);
+	struct framebuffer_pixel blue = {255, 0, 0, 0};
+	for (int y = 0; y < graphics->height; y++)
+	{
+		for (int x = 0; x < graphics->width; x++)
+		{
+			pixels[y * graphics->width + x] = blue;
+		}
+	}
+
+	myos_window_redraw(win);
 
 	while (1)
 	{
